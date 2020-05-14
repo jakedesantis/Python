24f809e1ca6cb08470c1e3d27d2882f7164e8e21
Jake DeSantis <jake.desantis@gmail.com>
2020-05-14 14:09:15 -0400

    Add files via upload

--- jakefunctions.py
+++ jakefunctions.py
@@ -166,7 +166,6 @@ def exponentialdecay(t, start, end, speed, plot):
     speed determines percent move from current towards end for each "t"
         for example speed=0.02 moves 2% closer to end each "t"
     plot is 1 (plot) or 0 (do not plot)
-    works?
     """
     import numpy as np
     if plot == 1:
@@ -176,3 +175,20 @@ def exponentialdecay(t, start, end, speed, plot):
         map(exponentialdecay, t)
     return start*np.exp(-speed*t)+end*(1-np.exp(-speed*t))
 
+def logistictransition(t, start, end, speed, midpoint, plot):
+    """t can be a positive scalar or vector of positives
+    start is a starting float or int
+    end is an ending float or int
+    speed determines percent move from current towards end for each "t"
+        for example speed=0.02 moves 2% closer to end each "t"
+    midpoint is the time at which transition is 50% complete
+    plot is 1 (plot) or 0 (do not plot)
+    """
+    import numpy as np
+    if plot == 1:
+        import matplotlib.pyplot as plt
+        plt.plot(t, start + (end-start)/(1+np.exp(-speed*(t-midpoint))))
+        plt.show()
+        map(logistictransition, t)
+    #return start*np.exp(-speed*t)+end*(1-np.exp(-speed*t))
+    return start + (end-start)/(1+np.exp(-speed*(t-midpoint)))
